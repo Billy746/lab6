e9a7ddb2beeff406055bd7d18659cd9bc1ce17c0
Ayomide Oguntokun <ay884120@dal.ca>
2025-03-13 19:45:22 -0300

    Lab6

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import NotFound from "./pages/NotFound";
-import "./css/styles.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import NotFound from './components/NotFound';
+import './styles.css';
 
-function App() {
+const App = () => {
   return (
     <Router>
-      {/* Skip to content link for keyboard users */}
-      <a href="#main-content" className="skip-to-content">
-        Skip to main content
-      </a>
-      
       <Header />
-      
-      <main id="main-content" className="container">
+      <main>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -26,10 +20,9 @@ function App() {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
-      
       <Footer />
     </Router>
   );
-}
+};
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-function Header() {
+const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3" aria-label="Main navigation">
-      <div className="container">
-        <Link className="navbar-brand" to="/">My Portfolio</Link>
-        
-        <button 
-          className="navbar-toggler" 
-          type="button" 
-          data-bs-toggle="collapse" 
-          data-bs-target="#navbarMain" 
-          aria-controls="navbarMain" 
-          aria-expanded="false" 
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        
-        <div className="collapse navbar-collapse" id="navbarMain">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/" aria-current="page">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/projects">Projects</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <header>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/projects">Projects</Link></li>
+        </ul>
+      </nav>
+    </header>
   );
-}
+};
 
 export default Header;
